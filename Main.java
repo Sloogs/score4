54ebd6c44cc573b0145d3dfa9c1b505bc6355e4d
Nick Slugocki <slugoc0@unbc.ca>
2020-03-05 00:30:31 -0800

    Some tests for add bead stuff

--- Main.java
+++ Main.java
@@ -1,32 +1,36 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Main
 {
 	public static void main(String[] args)
 	{
+		Debug.enable(); // This enables debugging messages.
 		System.out.println("Welcome to Score 4!");
-		// Debug.enable(); // This enables debugging messages.
+
 		Debug.log("Initalizing game and creating elements.");
 		Board board = new Board();
+
 		BoardTestViewer boardTestViewer = new BoardTestViewer(board);
-		Player player = new Player(Colour.WHITE);
-		player.placeBead(board, 1,1);
-		player.placeBead(board, 1,1);
-		player.placeBead(board, 1,1);
-		player.placeBead(board, 1,1);
-		player.placeBead(board, 1,1);
-		Peg[][] pegs = board.getGrid().getAllPegs();
+
+		Player player1 = new Player(Colour.WHITE);
+		Player player2 = new Player(Colour.BLACK);
 
 		Scanner input = new Scanner(System.in);
-
 		System.out.println("Please type a command: ");
-
 		boolean running = true;
 		while (running) {
 			String inputString = input.nextLine();
-			if (inputString.contains("show beads")) {
-				showBeads(pegs);
+			if (inputString.startsWith("add")) {
+				String pattern = "add (black|white) bead to ([A-D][1-4]).";
+				Pattern p = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
+				Matcher m = p.matcher(inputString);
+				m.matches();
+
+				System.out.println(m.group(1));
+				System.out.println(m.group(2));
 			}
 			else if (inputString.contains("show board"))
 			{
@@ -36,23 +40,6 @@ public class Main
 			{
 				running = false;
 			}
-		}
-	}
-
-	public static void showBeads(Peg[][] pegs) {
-		for (Peg[] row: pegs)
-		{
-			for(Peg peg: row)
-			{
-				ArrayList<Bead> beads = peg.getAllBeads();
-				for (Bead bead: beads)
-				{
-					System.out.println("Bead (id: " + bead.getId()
-							+ ") at: (x: " + bead.getXCoord()
-							+ ", y:" + bead.getYCoord() + ")");
-				}
-			}
-
 		}
 	}
 }
